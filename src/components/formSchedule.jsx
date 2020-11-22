5e7a6f6bae8bf346bb95ce59c14f735c32d3e42e
DiegoElgart <DiegoElgart>
2020-11-22 16:03:43 +0200

    failed to backup work for somedays, so backupin it

--- src/components/formSchedule.jsx
+++ src/components/formSchedule.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import dogService from "../services/dogService";
-import DatePicker from "react-datepicker";
-
-import "react-datepicker/dist/react-datepicker.css";
 
 export class FormSchedule extends Component {
   state = {
-    dogs: {},
-    visit: {},
-    date: new Date(),
+    dogs: [],
+    visit: {
+      dogId: "",
+      startAt: new Date(),
+      endAt: new Date(),
+    },
   };
   async componentDidMount() {
     const { data } = await dogService.getDog();
@@ -16,81 +16,23 @@ export class FormSchedule extends Component {
   }
   handleSubmit = e => {
     e.preventDefault();
-    const { dogs, date } = this.state;
-    console.log(dogs);
-    let newVisit = dogs + date;
-    console.log(newVisit);
-  };
-  handleDogChange = dog => {
-    this.setState({ dogs: dog.target.value });
-    console.log(dog.target.value);
-  };
-  handleChange = date => {
-    console.log(date);
-    this.setState({ date: date });
   };
 
   render() {
-    const { dogs, date } = this.state;
-
     return (
-      <div className='container m-3 ='>
-        <div className='row'>
-          <div className='form-group'>
-            <form onSubmit={this.handleSubmit}>
-              <label>Select your Doggie: </label>
-              <select onChange={this.handleDogChange} value={dogs}>
-                {dogs.length > 0 &&
-                  dogs.map(dog => (
-                    <option
-                      key={dog.dogId}
-                      dog={dog}
-                      value={dog.dogId}
-                      label={dog.dogName}
-                    />
-                  ))}
-              </select>
-              <br />
-              <div className='row'>
-                <div className='col-5 mt-3'>
-                  <DatePicker
-                    selected={date}
-                    onChange={this.handleChange}
-                    timeInputLabel='Time:'
-                    dateFormat='dd/MM/yyyy'
-                  />
-                  <br />
-                  <label>From :</label>
-                  <DatePicker
-                    selected={date}
-                    onChange={this.handleChange}
-                    showTimeSelect
-                    showTimeSelectOnly
-                    timeCaption='Time'
-                    dateFormat='h:mm aa'
-                  />
-                  <label>Until :</label>
-                  <DatePicker
-                    selected={date}
-                    onChange={this.handleChange}
-                    showTimeSelect
-                    showTimeSelectOnly
-                    timeCaption='Time'
-                    dateFormat='h:mm aa'
-                  />
-                </div>
-              </div>
-              <div className='form-group'>
-                <button className='btn btn-info badge-pill m-2'>
-                  Add Visit
-                </button>
-              </div>
-            </form>
-          </div>
+      <form>
+        <div className='form-group'>
+          <label>Choose your Dog!</label>
+          <select className='form-control'>
+            option
+          </select>
         </div>
-      </div>
+        <div className='form-group'>
+          <label className='m-3'>From: </label>
+          <input type='date' />
+        </div>
+      </form>
     );
   }
 }
-
 export default FormSchedule;
